75d3a46623845d877affae0d0a0064d4728d9a0f
AskaSlip <varenaska@gmail.com>
2024-10-24 22:05:42 +0400

    less7

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -1,27 +1,40 @@
-import { Column, Entity, PrimaryGeneratedColumn, VirtualColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('users')
-export class UserEntity {
+import { ArticleEntity } from './article.entity';
+import { TableNameEnum } from './enums/table-name.enum';
+import { LikeEntity } from './like.entity';
+import { CreateUpdateModel } from './models/create-update.model';
+import { RefreshTokenEntity } from './refresh-token.entity';
+
+@Entity(TableNameEnum.USERS)
+export class UserEntity extends CreateUpdateModel {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column('text')
+  name: string;
 
   @Column('text', { unique: true })
   email: string;
 
   @Column('text')
-  firstName: string;
-
-  @Column('text')
-  lastName: string;
+  password: string;
 
   @Column('boolean', { default: true })
   isActive: boolean;
 
-  @Column({ nullable: true, type: 'text' })
+  @Column('text', { nullable: true })
   bio: string;
 
-  @VirtualColumn({
-    query: () => `SELECT CONCAT(firstname, lastname) FROM users WHERE id = id)`,
-  })
-  fullname: string;
+  @Column('text', { nullable: true })
+  image: string;
+
+  @OneToMany(() => RefreshTokenEntity, (entity) => entity.user)
+  refreshTokens?: RefreshTokenEntity[];
+
+  @OneToMany(() => ArticleEntity, (entity) => entity.user)
+  articles?: ArticleEntity[];
+
+  @OneToMany(() => LikeEntity, (entity) => entity.user)
+  likes?: LikeEntity[];
 }
--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,18 +1,25 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Repository } from 'typeorm';
 
 import { AppConfig, Config } from '../../../configs/config.type';
+import { UserEntity } from '../../../database/entities/user.entity';
+import { UserRepository } from '../../repository/services/user.repository';
 import { CreateUserReqDto } from '../models/dto/req/create-user-req.dto';
 import { UpdateUserReqDto } from '../models/dto/req/update-user-req.dto';
 import { UsersResDto } from '../models/dto/res/user-res.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly configService: ConfigService<Config>) {}
+  constructor(
+    private readonly configService: ConfigService<Config>,
+    private usersRepository: UserRepository,
+  ) {}
 
   public async create(createUserDto: CreateUserReqDto): Promise<UsersResDto> {
     const appConfig = this.configService.get<AppConfig>('database');
     throw new ForbiddenException('jjj');
+
     return {} as UsersResDto;
   }
 
